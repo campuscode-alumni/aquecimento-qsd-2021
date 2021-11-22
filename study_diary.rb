e106b9ce0d6738fec94ebc13954c128bfdccc62a
Henrique Morato <riquemorato@gmail.com>
2021-11-22 19:53:32 -0300

    Organização de código

--- study_diary.rb
+++ study_diary.rb
@@ -1,5 +1,11 @@
 require 'io/console'
 
+INSERT = 1
+VIEW_ALL = 2
+SEARCH = 3
+MARK_AS_DONE = 4
+EXIT = 5
+
 def welcome
   'Boas-vindas ao Diário de Estudos, seu companheiro para estudar!'
 end
@@ -7,11 +13,11 @@ end
 def menu
   puts <<~MENU
     -----------------------------------------
-    [1] Cadastrar um item para estudar
-    [2] Ver todos os itens cadastrados
-    [3] Buscar um item de estudo
-    [4] Marcar um item como concluído
-    [5] Sair
+    [#{INSERT}] Cadastrar um item para estudar
+    [#{VIEW_ALL}] Ver todos os itens cadastrados
+    [#{SEARCH}] Buscar um item de estudo
+    [#{MARK_AS_DONE}] Marcar um item como concluído
+    [#{EXIT}] Sair
     -----------------------------------------
   MENU
   print 'Escolha uma opção: '
@@ -28,7 +34,7 @@ def create_study_item
 end
 
 def clear
-  system('clear')
+  system 'clear'
 end
 
 def wait_keypress
@@ -42,49 +48,54 @@ def wait_and_clear
   clear
 end
 
+def print_study_items(collection)
+  collection.each.with_index(1) do |item, index|
+    puts "##{index} - #{item[:title]} - #{item[:category]}#{' - Finalizada' if item[:done]}"
+  end
+  puts 'Nenhum item encontrado' if collection.empty?
+end
+
+def search_study_items(collection)
+  print 'Digite uma palavra para procurar: '
+  term = gets.chomp
+  collection.filter do |item|
+    item[:title].include? term
+  end
+end
+
+def mark_study_item_as_done(study_items)
+  not_finalized = study_items.filter { |item| !item[:done] }
+  print_study_items(not_finalized)
+  return if not_finalized.empty?
+
+  print 'Digite o número que deseja finalizar: '
+  index = gets.to_i
+  not_finalized[index - 1][:done] = true
+end
+
 clear
 puts welcome
 
 study_items = []
 option = menu
 
-while option != 5
-  if option == 1
+loop do
+  case option
+  when INSERT
     study_items << create_study_item
-    wait_and_clear
-  elsif option == 2
-    study_items.each.with_index(1) do |item, index|
-      puts "##{index} - #{item[:title]} - #{item[:category]}#{' - Finalizada' if item[:done]}"
-    end
-    puts 'Nenhum item cadastrado' if study_items.empty?
-    wait_and_clear
-  elsif option == 3
-    print 'Digite uma palavra para procurar: '
-    term = gets.chomp
-    found_items = study_items.filter do |item|
-      item[:title].include? term
-    end
-    puts found_items
-    puts 'Nenhum item encontrado' if found_items.empty?
-    wait_and_clear
-  elsif option == 4
-    not_finalized = study_items.filter { |item| !item[:done] }
-    not_finalized.each.with_index(1) do |item, index|
-      puts "##{index} - #{item[:title]} - #{item[:category]}#{' - Finalizada' if item[:done]}"
-    end
-    if study_items.empty?
-      puts 'Nenhum item encontrado'
-    else
-      print 'Digite o número que deseja finalizar: '
-      index = gets.to_i
-      not_finalized[index.to_i - 1][:done] = true
-    end
-    wait_and_clear
+  when VIEW_ALL
+    print_study_items(study_items)
+  when SEARCH
+    found_items = search_study_items(study_items)
+    print_study_items(found_items)
+  when MARK_AS_DONE
+    mark_study_item_as_done(study_items)
+  when EXIT
+    break
   else
     puts 'Opção inválida'
-    wait_and_clear
   end
-  clear
+  wait_and_clear
   option = menu
 end
 
